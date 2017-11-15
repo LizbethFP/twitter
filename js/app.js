03c6709668e7362807f459c754492eb4ada46ec9
LizbethFP <lizbeth.fp@gmail.com>
2017-11-14 21:57:16 -0500

    subiendo version 2

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 // Agregar evento de click  al botón
 var submit = btn.addEventListener('click', gettingText);
 var tweetBox = document.getElementById('tweet-box');
+var container = document.getElementById('container-tweets');
 
 
 function gettingText(){
@@ -10,8 +11,17 @@ function gettingText(){
   alert(tweetCaught);
 }
 
+var tweetCaught; //sacar el texto ingresado en la función en una variable
 
-// Creando y agregando tweet a HTML
-var container = document.getElementByClass('container-tweets');
-container.appendChild(gettingText);
-// addTweetToHtml = container-tweets.createElement(tweetCaught);
+// var texting = function gettingText();
+//Creando y agregando tweet a HTML
+var containerText = document.createTextNode(tweetCaught);
+container.appendChild(containerText);
+
+// Version 0.0.2
+//No ingresar texto vacío (deshabilitar el botón de "twittear").
+if (document.getElementById("tweet-box").value == ''){
+    // alert("debug");
+    document.getElementById("btn").style.display ="none";
+}
+//Contar la cantidad de caracteres de forma regresiva.
