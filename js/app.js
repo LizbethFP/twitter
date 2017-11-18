35a7140896d3d9abdb85e23a63d61dc9d3f70118
LizbethFP <lizbeth.fp@gmail.com>
2017-11-18 13:10:01 -0500

    subiendo versión 1 y 2 comentadas

--- js/app.js
+++ js/app.js
@@ -1,60 +1,41 @@
-// Version 0.0.1
-var tweetCaught = ""
-var tweetBox = document.getElementById('tweet-box');
+/* ************VERSION 0.0.1*************** */
+// Variables globales
+var button = document.querySelector('.btn');
+var textAreaPress = document.querySelector('.tweet-box');
 
-// Agregar evento de click  al botón, crear y agregar tweet a HTMl
-document.getElementById('btn').onclick = function(){
-
-  tweetCaught = tweetBox.value;
-  var paragraph = document.createElement("p");
-  var paragraphText = document.createTextNode(tweetCaught);
-  paragraph.appendChild(paragraphText);
-
-  var container = document.getElementById("test");
-  container.appendChild(paragraph);
-
+// Agregar un evento de click al botón o de submit al formulario.
+function tweet(event) {
+  // prevenir algunas acciones por defecto
+  event.preventDefault();
+  // En el evento, obtener el texto.
+  var capturedText = (document.querySelector('.tweet-box')).value;
+  var paragraphForCapturedText = document.createElement('p');
+  // Agregando el texto al html
+  paragraphForCapturedText.textContent = capturedText;
+  document.querySelector('.container-tweets').appendChild(paragraphForCapturedText);
 }
-// // Version 0.0.1
-// // Agregar evento de click  al botón
-// var boton = document.getElementById('btn');
-// var tweetBox = document.getElementById('tweet-box');
-// var container = document.getElementById('container-tweets');
-//
-// boton.addEventListener('click', gettingText);
-//
-// function gettingText(){
-//   var tweetCaught = tweetBox.value;
-//   alert(tweetCaught);
-//   createText(event);
-// }
-//
-// //Creando y agregando tweet a HTML
-// function createText{
-// var containerText = document.createTextNode(tweetCaught);
-// container.appendChild(containerText);
-// }
-//
-// // var tweetCaught; //sacar el texto ingresado en la función en una variable
-// // console.log(tweetCaught);
-// // var texting = function gettingText();
 
 
-// Version 0.0.2
-//No ingresar texto vacío (deshabilitar el botón de "twittear").
-// if (document.getElementById("tweet-box").value == ''){
-//     // alert("debug");
-//     document.getElementById("btn").style.disable ="false";
-// }
-// //Contar la cantidad de caracteres de forma regresiva.
-// var textarea = document.querySelector("textarea");
-//
-// textarea.addEventListener("input", function(){
-//     var maxlength = this.getAttribute("maxlength");
-//     var currentLength = this.value.length;
-//
-//     if( currentLength >= maxlength ){
-//         console.log("You have reached the maximum number of characters.");
-//     }else{
-//         console.log(maxlength - currentLength + " chars left");
-//     }
-// });
+/* ************VERSION 0.0.2*************** */
+// No ingresar texto vacío (deshabilitar el botón de "twittear").
+function validate() {
+  if (document.querySelector('.tweet-box').value === '') {
+    button.disabled = true;
+  } else {
+    button.disabled = false;
+  }
+}
+// Evento de click al boton en v1
+button.addEventListener('click', tweet);
+// Evento de keyup al boton en v2
+textAreaPress.addEventListener('keyup', validate);
+validate();
+textAreaPress.addEventListener('keyup', countCharactersDown);
+// Contar la cantidad de caracteres de forma regresiva.
+function countCharactersDown() {
+  var lengthText = 140 - (document.querySelector('.tweet-box').value.length);
+  document.querySelector('.characters').textContent = lengthText;
+}
+
+
+/* ***************VERSION 0.0.3********************** */
